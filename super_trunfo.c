c3b87c9fb76a84aa0a169605c03b4a54c0f95b5b
mirandaroberto <mirandafilho28@gmail.com>
2025-08-14 16:58:22 -0300

    Add files via upload

---
+++ super_trunfo.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    char est[2];
+    char cod[3];
+    char nc [20];
+    float ar, pib;
+    int pt;
+    
+    printf ("Qual estado. (abreviado)");
+        scanf ('%s', &est);
+
+    printf ("Digite um codigo (Até 03 numeros)");
+        scanf ('%d', &cod);
+    
+    printf ("Nome da capital");
+        scanf ('%s', &nc);
+    
+    printf ("Qual o tamanho do estado?");
+        scanf ('%d', &ar);
+    
+    printf ("Qual o PIB do estado?");
+        scanf ('%d', &pib);
+    
+    printf ("Quantos pontos turisticos tem a capital?");
+        scanf ('%d', &pt);
+        
+
+
+
+
+}
